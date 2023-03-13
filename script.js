b1fc6b71950d2781e2a66a7beb22ade8ea933179
Lucas Martins <122373045+MartinsLucass@users.noreply.github.com>
2023-03-13 00:16:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () =>{
+    const form = document.querySelector('body');
+   if(mode.classList.contains('fa-moon')){
+    mode.classList.remove('fa-moon');
+    mode.classList.add('fa-sun');
+    form.classList.add('dark');
+    return;
+   } 
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon');
+    form.classList.remove('dark');    
+});
